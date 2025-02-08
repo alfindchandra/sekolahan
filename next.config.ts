936426996f2573f90b6c6ade07d09a13c3ff7fac
alfindchandra <alfindwichandra55@gmail.com>
2025-02-08 15:05:51 +0700

    baru

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {},
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
